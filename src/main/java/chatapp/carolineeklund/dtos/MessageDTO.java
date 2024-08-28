33944c3794ec97a55d21d25a6b5bfd0f65da77dc
bycaroline <eklund.caroline@gmail.com>
2024-08-28 10:31:05 +0200

    adding messages done

--- src/main/java/chatapp/carolineeklund/dtos/MessageDTO.java
+++ src/main/java/chatapp/carolineeklund/dtos/MessageDTO.java
@@ -1,29 +1,14 @@
 package chatapp.carolineeklund.dtos;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class MessageDTO {
     private String id;
-    private String sender;
+    private String senderId;
     private String content;
     private String timestamp;
 
-    // Getter och Setter metoder
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
 }
